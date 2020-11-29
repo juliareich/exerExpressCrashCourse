e90f26c0fbc0f6279889c0cf28bd2dd48e2cadc4
juliareich <jareich23@gmail.com>
2020-11-29 16:48:40 -0700

    Completed express crash course

---
+++ Members.js
@@ -0,0 +1,23 @@
+
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'johndoe@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Bob Smith',
+        email: 'bobsmith@gmail.com',
+        status: 'inactive'  
+    },
+    {
+        id: 3,
+        name: 'Shannon Jones',
+        email: 'shannonjones@gmail.com',
+        status: 'active'  
+    }
+];
+
+module.exports = members;
